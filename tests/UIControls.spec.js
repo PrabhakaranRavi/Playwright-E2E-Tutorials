a162072143a7137a35df642cef8fce5ecc45bac4
PrabhakaranRavi <prabhakaranravi93@gmail.com>
2024-11-18 11:37:49 +0530

    Update UIControls.spec.js

--- tests/UIControls.spec.js
+++ tests/UIControls.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require("@playwright/test");
+const exp = require("constants");
 
 test("Entering wrong username and password", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
@@ -9,7 +10,19 @@ test("Entering wrong username and password", async ({ page }) => {
     const signInBtn = page.locator(`#username`)
     const selectOption = page.locator(`select.form-control`)
     await selectOption.selectOption(`consult`);
-    await page.locator(`.radiotextsty`).nth(2).check()
+    await page.locator(`.radiotextsty`).last().check()
     await page.locator(`#okayBtn`).click()
+
+    console.log(await page.locator(`.radiotextsty`).last().isChecked())
+    await expect(page.locator(`.radiotextsty`).last()).toBeChecked();
+
+    await page.locator(`#terms`).click();
+    await expect(page.locator(`#terms`)).toBeChecked();
+
+    await page.locator(`#terms`).uncheck();
+    expect(await page.locator(`#terms`).isChecked()).toBeFalsy();
+
+    const pageLocator = page.locator(`[href*='documents-request']`);
+    await expect(pageLocator).toHaveAttribute(`class`, `blinkingText`)
 
 });
